76d07465692707527e0b2ecff4191fc05ad64f4e
Wendy C <chriscowendy@gmail.com>
2021-01-04 14:34:27 -0800

    starting on task1.js

---
+++ task1.js
@@ -0,0 +1,2 @@
+// Task 1: Translate the story into code.
+//Promise basics explained using my birthday
---
+++ task2.js
@@ -0,0 +1,12 @@
+// Task 2: Create a guessing game.
+
+// User story: User can enter a number
+// User story: The system pick a random number from 1 to 6
+// User story: If user number is equal to a random number, give user 2 points
+// User story: If the user number is different from the random number by 1,
+// give the user 1 point, otherwise, give the user 0 points.
+// User story: User can decide to play the game as long as they want to
+
+const start = () => {};
+
+start();
