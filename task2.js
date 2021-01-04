56565c62e4dd30e797bf9c830d6bac676c36c523
Wendy C <chriscowendy@gmail.com>
2021-01-04 15:44:05 -0800

    Done with task2.js

--- task2.js
+++ task2.js
@@ -7,6 +7,67 @@
 // give the user 1 point, otherwise, give the user 0 points.
 // User story: User can decide to play the game as long as they want to
 
+const enterNumber = () => {
+    return new Promise((resolve, reject) => {
+        const userNumber = Number(window.prompt("Enter a number (1 - 6):"));
+        const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+        if (isNaN(userNumber)) {
+            reject(new Error ("Wrong Input Type"));
+        }
+
+        if (userNumber === randomNumber) {
+            resolve({
+                points: 2,
+                randomNumber,
+            });
+        } else if (
+            userNumber === randomNumber - 1 ||
+            userNumber === randomNumber + 1
+        ) {
+            resolve({
+                points: 1,
+                randomNumber,
+            });
+        } else {
+            resolve({
+                points: 0,
+                randomNumber,
+            });
+        }
+    });
+};
+
+const continueGame = () => {
+    return new Promise((resolve) => {
+        if (window.confirm('Do you want to continue?')) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+};
+
+const handleGuess = async () => {
+    try {
+        const result = await enterNumber();
+
+        alert (`Dice: ${result.randomNumber}: you got ${result.points} points`);
+
+        const isContinuing = await continueGame();
+
+        if (isContinuing) {
+            handleGuess();
+        } else {
+            alert("Game ends")
+        }
+    } catch (error) {
+        alert(error);
+    }
+};
+
+handleGuess();
+
 const start = () => {};
 
 start();
