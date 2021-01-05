ef3af488a0d5d6d70db1480a93ce31090b32a1b1
Wendy C <chriscowendy@gmail.com>
2021-01-04 16:24:14 -0800

    Completed task3.js. It was not very long.

--- task3.js
+++ task3.js
@@ -1,2 +1,12 @@
 // Task 3: using https://restcountries.eu/ API,
 // get country where alpha3Code = col
+
+
+const fetchData = async () => {
+    const res = await fetch("https://restcountries.eu/rest/v2/alpha/col");
+
+    const country = await res.json();
+    console.log(country);
+}
+
+fetchData();
